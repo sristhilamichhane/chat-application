bd79d8815641754999930ad262df1ccb8766fd6a
Sristhi Lamichhane <59720998+sristhilamichhane@users.noreply.github.com>
2022-11-14 06:42:43 +0545

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -22,7 +22,7 @@ const Login = () => {
   return (
     <div className="formContainer">
       <div className="formWrapper">
-        <span className="logo">Wavy-wave</span>
+        <span className="logo">Chill-pill</span>
         <span className="title">Login</span>
         <form onSubmit={handleSubmit}>
           <input type="email" placeholder="email" />
